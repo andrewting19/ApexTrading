0e71498bb07cffbf142951ef6652ada9b37734ac
Andrew Ting <andrewtingnyc@gmail.com>
2021-02-13 17:39:04 -0500

    Apex v1.2 Forgot 2/12 performance result

--- src/app/performance/performance.component.ts
+++ src/app/performance/performance.component.ts
@@ -12,6 +12,7 @@ import { ModalService } from '../modal/modal.service';
 export class PerformanceComponent implements OnInit {
   openedImg: string = null;
   images = [
+    'alert_result_2.12.png',
     'alert_result_2.5.png',
     'alert_result_1.29.png',
     'alert_result3.png',
